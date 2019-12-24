a85bfe86cfff69c56498c2b1c4ac17f2924888ff
roverchen <rover.k.chen@gmail.com>
2019-12-24 16:27:22 +0800

    app.serve("0.0.0.0:3000").unwrap();

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,41 +1,112 @@
 // Copyright (c) The dgc.network
 // SPDX-License-Identifier: Apache-2.0
 
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(async_await, futures_api, await_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate serde_derive;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use tide::{error::ResultExt, response, App, Context, EndpointResult};
+use http::status::StatusCode;
+use std::sync::Mutex;
+
+#[derive(Serialize, Deserialize, Clone)]
+struct User {
+    name: String,
+    height: u32,
+}
+
+#[derive(Default)]
+struct Database {
+    contents: Mutex<Vec<User>>,
+}
+
+impl Database {
+    fn insert(&self, user: User) -> usize {
+        let mut table = self.contents.lock().unwrap();
+        table.push(user);
+        table.len() - 1
+    }
+
+    fn get_all(&self) -> Vec<User> {
+        self.contents.lock().unwrap().clone()
+    }
+
+    fn get(&self, id: usize) -> Option<User> {
+        self.contents.lock().unwrap().get(id).cloned()
+    }
+
+    fn set(&self, id: usize, user: User) -> bool {
+        let mut table = self.contents.lock().unwrap();
+
+        if let Some(old_user) = table.get_mut(id) {
+            *old_user = user;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn delete(&self, id: usize) -> bool {
+        let mut table = self.contents.lock().unwrap();
+
+        if let Some(_user) = table.get_mut(id) {
+            self.contents.lock().unwrap().remove(id);
+            true
+        } else {
+            false
+        }
+    }
+}
+
+async fn handle_get_users(cx: Context<Database>) -> EndpointResult {
+    Ok(response::json(cx.app_data().get_all()))
+}
+
+async fn handle_get_user(cx: Context<Database>) -> EndpointResult {
+    let id = cx.param("id").client_err()?;
+    if let Some(user) = cx.app_data().get(id) {
+        Ok(response::json(user))
+    } else {
+        Err(StatusCode::NOT_FOUND)?
+    }
+}
+
+async fn handle_update_user(mut cx: Context<Database>) -> EndpointResult<()> {
+    let user = await!(cx.body_json()).client_err()?;
+    let id = cx.param("id").client_err()?;
+
+    if cx.app_data().set(id, user) {
+        Ok(())
+    } else {
+        Err(StatusCode::NOT_FOUND)?
+    }
+}
+
+async fn handle_create_user(mut cx: Context<Database>) -> EndpointResult<String> {
+    let user = await!(cx.body_json()).client_err()?;
+    Ok(cx.app_data().insert(user).to_string())
+}
+
+async fn handle_delete_user(cx: Context<Database>) -> EndpointResult<String> {
+    let id = cx.param("id").client_err()?;
+    Ok(cx.app_data().delete(id).to_string())
 }
 
 fn main() {
-    let (allowed_origins, failed_origins) = AllowedOrigins::some(&["http://localhost:3000"]);
-    assert!(failed_origins.is_empty());
+    let mut app = App::new(Database::default());
+    app.at("/users")
+        .post(handle_create_user)
+        .get(handle_get_users);
+    app.at("/users/:id")
+        .get(handle_get_user)
+        .patch(handle_update_user)
+        .delete(handle_delete_user);
 
-    let options = rocket_cors::Cors {
-        allowed_origins: allowed_origins,
-        allowed_methods: vec![Method::Get, Method::Post, Method::Options].into_iter().map(From::from).collect(),
-        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Content-Type"]),
-        allow_credentials: true,
-        ..Default::default()
-    };
-/*
-    let database_url = if let Ok(s) = env::var("DATABASE_URL") {
-        s
-    } else {
-        "postgres://localhost:5432".into()
-    };
-*/
-    let validator_url = if let Ok(s) = env::var("VALIDATOR_URL") {
-       s
-    } else {
-        "tcp://localhost:8004".into()
-    };
-
-    rocket::ignite().mount("/", routes![index]).launch();
+    //app.serve("127.0.0.1:8000").unwrap();
+    app.serve("0.0.0.0:3000").unwrap();
 }
+
 /*
 //#![feature(plugin, decl_macro, custom_derive)]
 #![feature(plugin, decl_macro)]
