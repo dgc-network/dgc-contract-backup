8b90e69bd23100198e269f981e0333f7e3d0c33f
roverchen <rover.k.chen@gmail.com>
2019-12-25 09:50:20 +0800

    write a rust program to serve our API.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,93 +1,24 @@
 // Copyright (c) The dgc.network
 // SPDX-License-Identifier: Apache-2.0
 
-extern crate dgc_contract_api;
-extern crate serde;
-#[macro_use] extern crate rouille;
+#![feature(proc_macro_hygiene, decl_macro)]
 
-use dgc_contract_api::{Task, TaskManager};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::sync::Mutex;
+#[macro_use]
+extern crate rocket;
 
-/// External representation of a single task.
-#[derive(Serialize)]
-struct GetTaskResponse<'a> {
-    text: &'a str,
-    done: bool,
-}
+use mytodo::db::{query_task, establish_connection};
 
-impl<'a> GetTaskResponse<'a> {
-    /// Constructs a new external version of a task given a task's definition.
-    fn from(t: &'a Task) -> Self {
-        Self { text: &t.text, done: t.done }
-    }
-}
-
-/// Representation of a request to update zero or more fields of a task.
-#[derive(Deserialize)]
-struct UpdateTaskRequest {
-    text: Option<String>,
-    done: Option<bool>,
-}
-
-/// Processes REST requests for the task manager API and transforms them into
-/// operations against the given backing `task_manager`.
-fn route_request(request: &rouille::Request, task_manager: &Mutex<TaskManager>)
-    -> rouille::Response {
-
-    let mut task_manager = task_manager.lock().unwrap();
-
-    router!(request,
-        (GET) ["/task"] => {
-            let mut response = HashMap::new();
-            for (id, task) in task_manager.all().iter() {
-                let path = format!("/task/{}", id);
-                response.insert(path, GetTaskResponse::from(task));
-            }
-            rouille::Response::json(&response)
-        },
-
-        (POST) ["/task"] => {
-            let body: String =
-                try_or_400!(rouille::input::json_input(request));
-            let id = task_manager.add(body);
-            rouille::Response::json(&format!("/task/{}", id))
-        },
-
-        (GET) ["/task/{id}", id: usize] => {
-            match task_manager.get(id) {
-                Ok(task) =>
-                    rouille::Response::json(&GetTaskResponse::from(task)),
-                Err(e) => rouille::Response::json(&e).with_status_code(404),
-            }
-        },
-
-        (UPDATE) ["/task/{id}", id: usize] => {
-            let body: UpdateTaskRequest =
-                try_or_400!(rouille::input::json_input(request));
-            match task_manager.set(id, body.text, body.done) {
-                Ok(()) => rouille::Response::empty_204(),
-                Err(e) => rouille::Response::json(&e).with_status_code(404),
-            }
-        },
-
-        (DELETE) ["/task/{id}", id: usize] => {
-            match task_manager.delete(id) {
-                Ok(()) => rouille::Response::empty_204(),
-                Err(e) => rouille::Response::json(&e).with_status_code(404),
-            }
-        },
-
-        _ => rouille::Response::empty_404()
-    )
+#[get("/tasks")]
+fn tasks_get() -> String {
+    "this is a response\n".into()
 }
 
 fn main() {
-    let task_manager = Mutex::from(TaskManager::new());
-    rouille::start_server(
-        "0.0.0.0:3000", move |request| route_request(request, &task_manager))
+    rocket::ignite()
+        .mount("/", routes![tasks_get])
+        .launch();
 }
+
 
 /*
 #![feature(async_await, futures_api, await_macro)]
