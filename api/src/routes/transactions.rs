0fa0aad85f87942786abcf468456e43bf20e6c01
roverchen <rover.k.chen@gmail.com>
2019-12-30 13:48:33 +0800

    query: TxnQuery) -> Result<Custom<Json<Vec<BatchStatus>>, Json>> {

--- api/src/routes/transactions.rs
+++ api/src/routes/transactions.rs
@@ -69,7 +69,7 @@ pub fn submit_txns_wait(
     data: Vec<u8>,
     //query: TxnQuery) -> Result<Custom<Json<Vec<BatchStatus>>>, Custom<Json>> {
     //query: TxnQuery) -> Result<Json<Vec<BatchStatus>>> {
-    query: TxnQuery) -> Result<Custom<Json<Vec<BatchStatus>>, <Json>> {
+    query: TxnQuery) -> Result<Custom<Json<Vec<BatchStatus>>, Json>> {
 
     let batch_status_list = submit_batches(&mut conn.clone(), &data, query.wait)
         .map_err(map_error)?;
